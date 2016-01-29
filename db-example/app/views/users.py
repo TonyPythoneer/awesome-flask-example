df81248e2790656d2df2f4aa186e0d36c3b1b063
TonyPythoneer <tonypythoneer@gmail.com>
2016-01-29 17:35:55 +0800

    Edit: Remove flask-restful resource

--- db-example/app/views/users.py
+++ db-example/app/views/users.py
@@ -5,17 +5,27 @@
 #  @version       0.0
 """Users API
 """
-from flask import Blueprint
-from flask_restful import Api, Resource
+from flask import Blueprint, Response, json
+from flask.views import MethodView
 
 from .. import db
+from ..models.users import User
+from ..data_formattings.users import signup_parser
 
 
 users_bp = Blueprint('users', __name__)
-api = Api(users_bp)
 
 
-@api.resource('/foo')
-class Foo(Resource):
-    def get(self):
-        return 'Hello, World!'
+class Signup(MethodView):
+    def post(self):
+        args = signup_parser.parse_args()
+        return 'OK', 200
+
+'''
+@users_bp.app_errorhandler(400)
+def fuck(err):
+    res_data = json.dumps(err.data)
+    return Response(response=res_data, status=400, mimetype="application/json")
+'''
+
+users_bp.add_url_rule('/signup', view_func=Signup.as_view('signup'))
