cf57b818661e989477ee9fe4d3497254f45c710c
TonyPythoneer <tonypythoneer@gmail.com>
2016-02-02 11:00:51 +0800

    Enhance: Add comments in models/mixins.py

--- db-example/app/models/mixins.py
+++ db-example/app/models/mixins.py
@@ -9,13 +9,18 @@ from .. import db
 
 
 class CRUDMixin(object):
+    """Simplify leveraging database"""
+
     def add(self):
+        """It's Create of CRUD."""
         db.session.add(self)
         db.session.commit()
 
     def update(self):
+        """It's Update of CRUD."""
         db.session.commit()
 
     def delete(self):
+        """It's Delte of CRUD."""
         db.session.delete(self)
         db.session.commit()
