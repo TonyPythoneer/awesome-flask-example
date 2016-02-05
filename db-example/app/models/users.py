f9f54d8cabf653470857f08b45b7fbfb72d622fa
TonyPythoneer <tonypythoneer@gmail.com>
2016-02-05 11:59:45 +0800

    Enhance: Finish token

--- db-example/app/models/users.py
+++ db-example/app/models/users.py
@@ -8,9 +8,10 @@
 Security Helpers about werkzeug with generate_password_hash and check_password_hash:
     http://werkzeug.pocoo.org/docs/0.11/utils/#module-werkzeug.security
 """
-import binascii
 from datetime import datetime, timedelta
-import os
+import random
+
+from flask import jsonify
 
 from flask.ext.login import UserMixin, make_secure_token
 
@@ -18,6 +19,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import mixins
 from .. import db, login_manager
+
+
+__all__ = ["User", "Token"]
 
 
 class User(UserMixin, mixins.CRUDMixin, db.Model):
@@ -37,8 +41,9 @@ class User(UserMixin, mixins.CRUDMixin, db.Model):
     # relationship
     devices = db.relationship('Device', backref='device', lazy='dynamic')
 
+    # Token relation fields
     last_login = db.Column(db.DateTime)
-    key = relationship("Token", uselist=False, backref=db.backref("User", uselist=False))
+    token = db.relationship("Token", uselist=False, backref="token")
 
     def __init__(self, email, password, nickname=None):
         self.email = email
@@ -48,14 +53,6 @@ class User(UserMixin, mixins.CRUDMixin, db.Model):
     def __repr__(self):
         return "<User(email='%s')>" % (self.email)
 
-    def set_password(self, password):
-        """The default method is sha1 to hash password"""
-        self.password = generate_password_hash(password)
-
-    def check_password(self, password):
-        """Verify password"""
-        return check_password_hash(self.password, password)
-
     @classmethod
     def authenticate(cls, email=None, password=None):
         """Get the user"""
@@ -64,19 +61,27 @@ class User(UserMixin, mixins.CRUDMixin, db.Model):
             return None
         return user
 
+    def set_password(self, password):
+        """The default method is sha1 to hash password"""
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        """Verify password"""
+        return check_password_hash(self.password, password)
+
     def login(self):
         self.last_login = datetime.utcnow()
-        token = Token.query.first(self)
-        if token:
-            return token
-        token.generate_key()
-        token.save()
-        return token
+        # Update token
+        if self.token:
+            self.token.generate_key()
+            self.token.update()
+        # Create token
+        token = Token(user_id=self.id)
+        token.add()
 
     def logout(self):
-        token = Token.query.first(self)
-        if token:
-            token.delete()
+        if self.token:
+            self.token.delete()
 
     def is_expired(self):
         return self.token.is_expired()
@@ -86,31 +91,47 @@ class Token(mixins.CRUDMixin, db.Model):
     __tablename__ = 'token'
     AUTH_TOKEN_DURATION = 60*60
 
-    key = db.Column(db.String(40), unique=True, index=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship("User", back_populates="Token")
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    key = db.Column(db.String(128), primary_key=True, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __init__(self, user_id):
+        self.user_id = user_id
 
     def generate_key(self):
-        self.key = binascii.hexlify(os.urandom(20)).decode()
-        return self.key
+        """Using make_secure_token
+
+        https://flask-login.readthedocs.org/en/latest/_modules/flask_login.html#make_secure_token
+        """
+        key = make_secure_token(str(self.user_id), key=str(random.random()))
+        self.key = key
 
     def is_expired(self):
         diff = datetime.utcnow - self.created_at
-        return diff > timedelta(seconds=self.AUTH_TOKEN_DURATION)
+        is_expired = diff > timedelta(seconds=self.AUTH_TOKEN_DURATION)
+        return is_expired
+
+    def add(self):
+        """It's Create of CRUD."""
+        self.generate_key()
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """It's Update of CRUD."""
+        self.generate_key()
+        db.session.commit()
 
 
 @login_manager.request_loader
 def load_request(request):
-    api_key = request.headers.get('Authorization', '')
-    if api_key:
-        user = User.query.filter_by(key=api_key).first()
-        if user:
-            return user
+    auth_token = request.headers.get('Authorization', '')
+    if auth_token:
+        user = User.query.filter(Token.key == auth_token).first()
+        return user
     return None
 
 
 @login_manager.unauthorized_handler
 def unauthorized_handler():
-    from flask import jsonify
     return jsonify({"message": "Unauthorized"}), 401
